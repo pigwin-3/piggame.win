7d7c2862cb26807359e066ad82c0288a6603edc3
Peder Vatn Austad <peder.vatn.austad@gmail.com>
2023-05-11 09:50:36 +0200

    dont wanna lose jack shit

--- funfax.php
+++ funfax.php
@@ -1,21 +1,64 @@
 <?php
+require 'config.php';
+
+$currentTime = date('Y-m-d H:i:s');
+echo "currentTime: " . $currentTime;
 // plan:
 
-// how long will facts be run
-// SELECT `setting-value` FROM `settings` WHERE `setting-name` = 'TimeBetweenFacts'
-// get current fact
-// SELECT `setting-value` FROM `settings` WHERE `setting-name` = 'CurentFact'
+// hvor lang tid vil en fakta kjøre
+$queryTimeBetweenFacts = "SELECT `setting-value` FROM `settings` WHERE `setting-name` = 'TimeBetweenFacts'";
+$resultTimeBetweenFacts = mysqli_query($con, $queryTimeBetweenFacts);
+$rowTimeBetweenFacts = mysqli_fetch_assoc($resultTimeBetweenFacts);
+$TimeBetweenFacts = $rowTimeBetweenFacts['setting-value'];
 
-//how long till a fact can be reused
-// SELECT `setting-value` FROM `settings` WHERE `setting-name` = 'CanReuseAfter'
+echo '<br>TimeBetweenFacts: ';
+echo $TimeBetweenFacts;
 
-// when was the fact first used
-// SELECT `last-used` FROM `facts` WHERE `id`=1
-// find out if its bean used for longer than the "TimeBetweenFacts"
+//hvor lang tid kan en fakta kan bli brukt på nytt
+$queryCanReuseAfter = "SELECT `setting-value` FROM `settings` WHERE `setting-name` = 'CanReuseAfter'";
+$resultCanReuseAfter = mysqli_query($con, $queryCanReuseAfter);
+$rowCanReuseAfter = mysqli_fetch_assoc($resultCanReuseAfter);
+$CanReuseAfter = $rowCanReuseAfter['setting-value'];
 
-// if it has not bean used longer than "TimeBetweenFacts" it will continue being used
-// SELECT `fact` FROM `facts` WHERE `id` = 1
-// echo fact
+echo '<br>CanReuseAfter: ';
+echo $CanReuseAfter;
+
+//hvilken fakta er i bruk nå
+$queryCurentFact = "SELECT `setting-value` FROM `settings` WHERE `setting-name` = 'CurentFact'";
+$resultCurentFact = mysqli_query($con, $queryCurentFact);
+$rowCurentFact = mysqli_fetch_assoc($resultCurentFact);
+$CurentFact = $rowCurentFact['setting-value'];
+
+echo '<br>CurentFact: ';
+echo $CurentFact;
+
+$queryLastUsed = "SELECT `last-used` FROM `facts` WHERE `id` = $CurentFact";
+$resultLastUsed = mysqli_query($con, $queryLastUsed);
+
+if ($resultLastUsed) {
+    $rowLastUsed = mysqli_fetch_assoc($resultLastUsed);
+    $lastUsed = $rowLastUsed['last-used'];
+
+    echo '<br>lastUsed: ';
+    echo $lastUsed;
+} else {
+    echo "Could not get fact";
+}
+
+// last used + TimeBetweenFacts > $currentTime
+
+$stopUsage = date("Y-m-d H:i:s", (strtotime(date($lastUsed)) + $TimeBetweenFacts));
+echo '<br>stopUsage: ';
+echo $stopUsage;
+
+echo '<br>stuff: ';
+if ($stopUsage > $currentTime){
+    // forset å bruke samme fakta
+    echo "smol";
+} else {
+    echo "big";
+    // skaff nytt fakta
+}
 
 
 // if it has it will select another fact where the fact has not gone over its reuseabliety
